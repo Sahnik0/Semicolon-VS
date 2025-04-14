021b769b82694a9928b302bc1b0d9382ba9a108f
sanks011 <sankalpasarkar68@gmail.com>
2025-04-14 12:32:39 +0530

    Home page design updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ParallaxProvider } from 'react-scroll-parallax'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <ParallaxProvider>
+    <App />
+  </ParallaxProvider>
+);
